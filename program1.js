ed4c3324c3a7dc55bdef6fe4b51a6f324780e21a
ankushjassal <47685511+ankushjassal@users.noreply.github.com>
2019-08-06 23:58:19 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,5 +1,6 @@
-<h2>Q3. What will the code below output? Explain your answer.
+Q3. What will the code below output? Explain your answer.
 console.log(0.1 + 0.2);
 console.log(0.1 + 0.2 == 0.3);
+Output:-
 0.30000000000000004
 false
